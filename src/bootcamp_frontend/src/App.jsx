d3ab03345dbb00ae5b2a17ae515a620ac1864b41
JoojoCoding <weichergames345@gmail.com>
2024-06-24 19:14:38 +0200

    react counter

--- src/bootcamp_frontend/src/App.jsx
+++ src/bootcamp_frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
-      what a sigma, skibidi ohio rizz
+      <button>-</button>{counter}<button onClick={() => setCounter((prev) => (prev+1))}>+</button>
 
     </main>
   );
