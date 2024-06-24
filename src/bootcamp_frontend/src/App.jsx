eae14d9023c1d2aad7671f925f77767fb0c4d94a
JoojoCoding <weichergames345@gmail.com>
2024-06-24 18:44:33 +0200

    first commit

---
+++ src/bootcamp_frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      what a sigma, skibidi ohio rizz
+
+    </main>
+  );
+}
+
+export default App;
